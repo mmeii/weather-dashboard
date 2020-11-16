ead1b38eab8d4de3c930da0a5a24fe149c646d78
Mengmei Tu <redmmeii@gmail.com>
2020-11-16 11:15:46 -0600

    add currentCondition function section

--- Assets/script.js
+++ Assets/script.js
@@ -24,24 +24,38 @@ var day3 = moment().add(3, 'days').calendar();
 var day4 = moment().add(4, 'days').calendar(); 
 var day5 = moment().add(5, 'days').calendar(); 
 
+
 // function for current condition
 function currentCondition(city) {
 
-    var queryURL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
+        $("#weatherContent").css("display", "block");
         console.log(response);
+        var iconCode = response.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
         // WHEN I view current weather conditions for that city
         // THEN I am presented with the city name
-
         // the date
         // an icon representation of weather conditions
         // the temperature
         // the humidity
         // the wind speed
+        const currentCity = $(`
+            <h2 id="currentCity">
+                ${response.name} ${today} <img src="${iconURL}" alt="current weather icon" />
+            </h2>
+            <p>Temperature: ${response.main.temp}</p>
+            <p>Humidity: ${response.main.humidity}</p>
+            <p>Wind Speed: ${response.wind.speed} MPH</p>
+        `);
+
+        $("#cityDetail").append(currentCity);
+
         // and the UV index
         // WHEN I view the UV index
         // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
@@ -62,7 +76,7 @@ function addtoHistory() {
 // function for future condition
 function futureCondition(city) {
 
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
+    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?q="${city}&units=imperial&appid=${apiKey}`;
 
     $.ajax({
         url: queryURL,
